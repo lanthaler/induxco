3737a1bcf2ddfcbe2f8c63f791ae94ca71c7a3b8
Markus Lanthaler <mark_lanthaler@gmx.net>
2016-03-28 23:03:07 +0200

    Hook up increase event to global listener

--- index.js
+++ index.js
@@ -33,14 +33,10 @@ const views = (state = 0, action) => {
 // ---------------- Renderer -------------------
 
 function renderIdom(state) {
-  let handler = function() {
-    store.dispatch(incrementCounter());
-  };
   return <div>
       <h1>This is the first <strong>IncrementalDOM</strong> demo</h1>
       <p>Counter value: {state}</p>
       <counter-card value={state}></counter-card>
-      <p><button onclick={handler}>Increment</button></p>
     </div>;
 }
 
@@ -56,6 +52,12 @@ let store = createStore(rootReducer);
 let unsubscribe = store.subscribe(() => {
   console.log(store.getState())
   IncrementalDOM.patch(document.body, renderIdom, store.getState().views);
+});
+
+// TODO: Event should be registered with an attribute on the counter-card element instead
+window.addEventListener('increase', (e) => {
+  e.preventDefault();
+  store.dispatch(incrementCounter());
 });
 
 store.dispatch(initializeApp());
