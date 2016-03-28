12fca5546cca0a372c1b224be02e315c3aa375df
Markus Lanthaler <mark_lanthaler@gmx.net>
2016-03-28 23:26:48 +0200

    Introduce a second counter-card and a new event for it

--- index.js
+++ index.js
@@ -15,15 +15,26 @@ function makeActionCreator(type, ...argNames) {
 const INITIALIZE_APP = 'INITIALIZE_APP';
 const initializeApp = makeActionCreator(INITIALIZE_APP);
 
-const INCREMENT_COUNTER = 'INCREMENT_COUNTER';
-const incrementCounter = makeActionCreator(INCREMENT_COUNTER);
+const INCREMENT_VIEWS = 'INCREMENT_VIEWS';
+const incrementViews = makeActionCreator(INCREMENT_VIEWS);
+
+const INCREMENT_VISITORS = 'INCREMENT_VISITORS';
+const incrementVisitors = makeActionCreator(INCREMENT_VISITORS);
 
 
 // ---------------- Reducers -------------------
 
 const views = (state = 0, action) => {
   switch (action.type) {
-    case INCREMENT_COUNTER:
+    case INCREMENT_VIEWS:
+      return state + 1;
+  }
+  return state;
+};
+
+const visitors = (state = 0, action) => {
+  switch (action.type) {
+    case INCREMENT_VISITORS:
       return state + 1;
   }
   return state;
@@ -35,8 +46,9 @@ const views = (state = 0, action) => {
 function renderIdom(state) {
   return <div>
       <h1>This is the first <strong>IncrementalDOM</strong> demo</h1>
-      <p>Counter value: {state}</p>
-      <counter-card value={state}></counter-card>
+      <p>Sum of views and visitors: {state.views + state.visitors}</p>
+      <counter-card heading="Views" value={state.views} onIncrement={incrementViews}></counter-card>
+      <counter-card heading="Visitors" value={state.visitors} onIncrement={incrementVisitors}></counter-card>
     </div>;
 }
 
@@ -46,18 +58,20 @@ function renderIdom(state) {
 import { createStore, combineReducers } from 'redux';
 import * as IncrementalDOM from 'incremental-dom';
 
-const rootReducer = combineReducers({views});
+const rootReducer = combineReducers({views, visitors});
 let store = createStore(rootReducer);
 
 let unsubscribe = store.subscribe(() => {
   console.log(store.getState())
-  IncrementalDOM.patch(document.body, renderIdom, store.getState().views);
+  IncrementalDOM.patch(document.body, renderIdom, store.getState());
 });
 
-// TODO: Event should be registered with an attribute on the counter-card element instead
+// TODO: Event listener should be registered automatically
 window.addEventListener('increase', (e) => {
-  e.preventDefault();
-  store.dispatch(incrementCounter());
+  if (e.target.onIncrement) {
+    e.preventDefault();
+    store.dispatch(e.target.onIncrement());
+  }
 });
 
 store.dispatch(initializeApp());
