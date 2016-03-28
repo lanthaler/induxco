bdb6a9b187934d79d9a0b39d3e506b4919bb8867
Markus Lanthaler <mark_lanthaler@gmx.net>
2016-03-28 17:32:49 +0200

    Initial import

---
+++ devserver.js
@@ -0,0 +1,25 @@
+var webpack = require('webpack')
+var webpackDevMiddleware = require('webpack-dev-middleware')
+var webpackHotMiddleware = require('webpack-hot-middleware')
+var config = require('./webpack.config')
+
+var app = new (require('express'))()
+var static = require('express').static;
+var port = 3000
+
+var compiler = webpack(config)
+app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
+app.use(webpackHotMiddleware(compiler))
+app.use('/bower_components', static('bower_components'));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.listen(port, function(error) {
+  if (error) {
+    console.error(error)
+  } else {
+    console.info("==> Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
+  }
+})
